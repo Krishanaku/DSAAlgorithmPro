9e294a58429cf241a9966b6f82880e012bf72442
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-28 13:26:22 +0530

    Add : Rewrite Use Generics for Search and Sort Algorithms

--- Program.cs
+++ Program.cs
@@ -2,53 +2,37 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void BubbleSort<T>(T[] arr) where T : IComparable<T>
         {
-            Console.WriteLine("Enter Lowest of value between 0 to 1000: ");
-            int start = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Lowest of value between 0 to 1000: ");
-            int end = int.Parse(Console.ReadLine());
-
-            for (int i = start; i <= end; i++)
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
             {
-                if (i % 2 == 0)
+                for (int j = 0; j < n - i - 1; j++)
                 {
-                    continue;
-                }
-                else
-                {
-                    Console.WriteLine("Prime Number : {0}", i);
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        T temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
                 }
             }
-
-            Console.WriteLine("Palindrome numbers in the range {0}, {1}:", start, end);
-
-            for (int num = start; num <= end; num++)
+            Console.WriteLine("Binary Sorted");
+            foreach (var p in arr)
             {
-                if (IsPalindrome(num))
-                {
-                    Console.WriteLine(num);
-                }
+                Console.WriteLine(p);
             }
-        }
-
-        public static bool IsPalindrome(int num)
-        {
-            int originalNum = num;
-            int reversedNum = 0;
-
-            while (num > 0)
-            {
-                int digit = num % 10;
-                reversedNum = reversedNum * 10 + digit;
-                num /= 10;
-            }
-
-            return originalNum == reversedNum;
 
 
         }
+        public static void Main(string[] args) 
+        {
+            int[] arr = { 5, 2, 8, 4, 1 };
+            Program.BubbleSort(arr);
+            
+            
+        }
+      
 
 
     }
