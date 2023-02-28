b2f1b8841f4727d72d0c8a1ded4650ca735ee748
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-28 15:57:38 +0530

    Add : Customize Message Demonstration using String Function and RegEx i. Replace <<name>> by first name of the user ( assume you are the user) ii. replace <<full name>> by user full name. iii. replace any occurance of mobile number that should be in format 91-xxxxxxxxxx by your contact number. iv. replace any date in the format XX/XX/XXXX by current date.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,57 @@
-﻿namespace DSAAlogrithmPro
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace DSAAlogrithmPro
 {
     public class Program
     {
 
-        public static void Display()
+        public static void DisplayFname()
         {
-            int i;
-            Console.WriteLine("Enter the N value : ");
-            int N = Convert.ToInt32(Console.ReadLine());
-            int[] arr = new int[N];
-            for (i = 0; i < N; i++)
-            {
-                var temp = (int)Math.Pow(2, i);
-                // Console.WriteLine(temp);
-                foreach (int x in arr)
-                {
-                    arr[i] = temp;
-                    Console.WriteLine(arr[i]);
-                    break;
+            string inputString = "<<name>> First Name ";
+            string firstName = "John"; // Replace with your first name
 
-                }
+            string outputString = Regex.Replace(inputString, "<<name>>", firstName);
 
-   
-            }
-            Console.WriteLine("Enter the Value to be Search in Array : ");
-            int user = Convert.ToInt32(Console.ReadLine());
-            for (int j = 0; j < arr.Length; j++)
-            {
-                if (arr[j] == user)
-                {
-                    
-                    Console.WriteLine("Number is exist");
-                    break;
-                }
-
-
-            }
+            Console.WriteLine(outputString);
         }
-        
+        public static void DisplayFullName()
+        {
+            string input = "Hello <<full name>>! How are you?";
+            string fullName = "John Doe"; // Replace this with the actual full name
+
+            string output = Regex.Replace(input, "<<full name>>", fullName);
+            Console.WriteLine(output); // Outputs: Hello John Doe! How are you?
+
+        }
+
+        public static void DisplayMobile() 
+        {
+            string input = "My mobile number is 91-9876543210. ";
+            string contactNumber = "1234567890"; 
+
+            string output = Regex.Replace(input, @"91-\d{10}", contactNumber);
+            Console.WriteLine(output); 
+        }
+        public static void DisplayCurrentDate()
+        {
+            string input = "Today's date is 02/28/2023";
+            DateTime currentDate = DateTime.Now.Date;
+
+            string output = Regex.Replace(input, @"\d{2}/\d{2}/\d{4}", currentDate.ToString("MM/dd/yyyy"));
+            Console.WriteLine(output); // Outputs: Today's date is 02/28/2023. Please confirm the order by 02/28/2023.
+
+        }
+
+
+
+
         public static void Main(string[] args) 
         {
-            Program.Display();
+            Program.DisplayFname();
+            Program.DisplayFullName();
+            Program.DisplayMobile();
+            Program.DisplayCurrentDate();
            
 
         }
